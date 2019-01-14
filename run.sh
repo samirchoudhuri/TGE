bf21f90981d4045ca3ca80bc8003e173b0a7c0a0
samirchoudhuri <46684209+samirchoudhuri@users.noreply.github.com>
2019-01-14 22:43:20 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,3 @@
-path="/data2/samir/Estimator/TGE_github/srcbin"
-export path
-
-$path/v2psfitsc ../visdiff.fits GV.fits input.v2psfitsm Mg.fits k2gg.fits k2gg1.fits 1
-$path/binv2psfitscm GV.fits input.v2psfitsm powerspec.dat Mg.fits 
+./v2psfitsc ../visdiff.fits GV.fits input.v2psfitsm Mg.fits k2gg.fits k2gg1.fits 1
+.//binv2psfitscm GV.fits input.v2psfitsm powerspec.dat Mg.fits 
 rm -rf k2gg*.fits
