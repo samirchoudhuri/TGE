6c9ba149c45af0c703e6a2af1b839465946dfc67
Samir Choudhuri <samir@astpc005.ph.qmul.ac.uk>
2020-12-31 17:10:02 +0000

    fixed bug in beam.s

--- beam.c
+++ beam.c
@@ -8,22 +8,19 @@ extern double D;
 
 double Beam(double pos,double freq)
 {
-  double D=45.,d=1.,nubyc,c;
-  double be,arg;
+  double nubyc,c,be;
   c=3.*pow(10,8.);//m/s
-  
+
   nubyc=freq/c;//m^-1
   
-  D=D*nubyc;
-  d=d*nubyc;
-  arg=M_PI*pos*D;
+
   if(pos==0.)
     {
       be=1.;
     }
   else
     {
-      be=(2.)*(gsl_sf_bessel_J1(M_PI*pos*D)/(M_PI*pos*D));
+      be=(2.)*(gsl_sf_bessel_J1(M_PI*pos*D*nubyc)/(M_PI*pos*D*nubyc));
 
       be=be*be;
     }
